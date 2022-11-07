7adf19fce5143773d2acd2498ee61d24551fa44c
Javier Castorena <javier.castorena@reconext.com>
2022-11-07 10:56:31 -0800

    erase ok

---
+++ build/CMakeFiles/eraser.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eraser.dir/build_device.c.o"
+  "CMakeFiles/eraser.dir/build_device.c.o.d"
+  "CMakeFiles/eraser.dir/config.c.o"
+  "CMakeFiles/eraser.dir/config.c.o.d"
+  "CMakeFiles/eraser.dir/erase_device.c.o"
+  "CMakeFiles/eraser.dir/erase_device.c.o.d"
+  "CMakeFiles/eraser.dir/erase_log.c.o"
+  "CMakeFiles/eraser.dir/erase_log.c.o.d"
+  "CMakeFiles/eraser.dir/gui.c.o"
+  "CMakeFiles/eraser.dir/gui.c.o.d"
+  "CMakeFiles/eraser.dir/leak_memory.c.o"
+  "CMakeFiles/eraser.dir/leak_memory.c.o.d"
+  "CMakeFiles/eraser.dir/main.c.o"
+  "CMakeFiles/eraser.dir/main.c.o.d"
+  "CMakeFiles/eraser.dir/sg_cmds.c.o"
+  "CMakeFiles/eraser.dir/sg_cmds.c.o.d"
+  "CMakeFiles/eraser.dir/sg_io_linux.c.o"
+  "CMakeFiles/eraser.dir/sg_io_linux.c.o.d"
+  "CMakeFiles/eraser.dir/sg_lib.c.o"
+  "CMakeFiles/eraser.dir/sg_lib.c.o.d"
+  "CMakeFiles/eraser.dir/sg_pt_linux.c.o"
+  "CMakeFiles/eraser.dir/sg_pt_linux.c.o.d"
+  "eraser"
+  "eraser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/eraser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/eraser.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for eraser.
+# This may be replaced when dependencies are built.
